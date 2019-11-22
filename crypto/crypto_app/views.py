230b6fb8afea2548a5485a8d0ffda8eaf9178af4
Hai The Dude <hainghiem375@gmail.com>
2019-11-22 16:19:06 -0330

    crypto-dashboard
    dashboard dashboard

---
+++ crypto/crypto_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    import requests
+    import json
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api})
