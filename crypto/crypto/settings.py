bcd8bc60a421cf3b0e7e3f1bd468604472e4e829
Hai The Dude <hainghiem375@gmail.com>
2019-11-24 00:49:34 -0330

    Update settings.py

--- crypto/crypto/settings.py
+++ crypto/crypto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h7oo0km!3g$nl$mcoy7e$jfkehp2@#fko7-pqvub4bcu^%p2yp'
+SECRET_KEY = 'NAHNAHNAH NO KEYS HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
