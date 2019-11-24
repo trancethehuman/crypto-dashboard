ca6fdbe50fc13869555880f3d9cfb0c0e30d72c7
Hai The Dude <hainghiem375@gmail.com>
2019-11-24 00:48:19 -0330

    error handlings completed

--- crypto/crypto_app/views.py
+++ crypto/crypto_app/views.py
@@ -4,7 +4,7 @@ def home(request):
     import requests
     import json
     #Price API
-    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BCH,LTC,XLM,ADA,USDT,MIOTA,TRX,EOSXRP&tsyms=CAD")
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BCH,LTC,XLM,ADA,USDT,MIOTA,TRX,EOS,XRP&tsyms=CAD")
     price = json.loads(price_request.content)
 
 
@@ -15,4 +15,14 @@ def home(request):
     return render(request, 'home.html', {'api': api, 'price': price})
 
 def prices(request):
-    return render(request, 'prices.html', {})
+    if request.method == "POST":
+        import requests
+        import json
+        quote = request.POST['quote']
+        quote = quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=CAD")
+        crypto = json.loads(crypto_request.content)
+        return render(request, 'prices.html', {'quote': quote, 'crypto':crypto})
+    else:
+        notfound = "Enter a crypto symbol into the search bar"
+        return render(request, 'prices.html', {'notfound': notfound})
